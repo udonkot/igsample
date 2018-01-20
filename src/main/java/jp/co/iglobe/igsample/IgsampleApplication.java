18c0e981ce732001ab27fb9fb76bbbd2555e67a7
ToshiyukiKondo <kondo@iglobe.co.jp>
2018-01-21 00:22:59 +0900

    springboot update
    templatefile update
    controller add

---
+++ src/main/java/jp/co/iglobe/igsample/IgsampleApplication.java
@@ -0,0 +1,12 @@
+package jp.co.iglobe.igsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgsampleApplication.class, args);
+	}
+}
---
+++ src/main/java/jp/co/iglobe/igsample/IgsampleApplicationController.java
@@ -0,0 +1,17 @@
+package jp.co.iglobe.igsample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IgsampleApplicationController {
+
+	@GetMapping("/top")
+	public String top(Model model) {
+		model.addAttribute("message", "hello thymeleaf");
+		return "top";
+	}
+
+
+}
